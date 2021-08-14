e0333cde2f598b101fdd122b23f18e962ed68ed7
Julius Schwartzenberg <julius.schwartzenberg@gmail.com>
2021-08-14 15:32:54 +0200

    Trigger SB16 driver installation after Windows installation

--- src/bat/inswin31.bat
+++ src/bat/inswin31.bat
@@ -2,3 +2,4 @@
 unix -s dosemu-preinstallwin31
 if exist %USERDRV%:\inst\win31\setup.exe %USERDRV%:\inst\win31\setup.exe /h:f:\win31\dosemu.shh
 if exist %USERDRV%:\windows\nul path %PATH%;%USERDRV%:\windows
+if exist %USERDRV%:\inst\win31drv\audio\disk1\install.exe %USERDRV%:\inst\win31drv\audio\disk1\install.exe
