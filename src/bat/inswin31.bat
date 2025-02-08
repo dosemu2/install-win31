25bbbf471ba9231c790f98d9366091e5c47b82c6
Julius Schwartzenberg <julius.schwartzenberg@eclipso.eu>
2025-02-08 22:43:45 +0100

    Do not scan for installed programs and just exit after installing.
    This fully automates the installation procedure.
    This also adds error handling to the script and clearer output.
    Get rid of unnecessary autoexec.bat and config.sys

--- src/bat/inswin31.bat
+++ src/bat/inswin31.bat
@@ -1,4 +1,19 @@
 @echo off
 unix -s dosemu-preinstallwin31
+if errorlevel 1 goto faildownload
 if exist %USERDRV%:\inst\win31\setup.exe %USERDRV%:\inst\win31\setup.exe /h:f:\win31\dosemu.shh
+if errorlevel 1 goto failwinsetup
 if exist %USERDRV%:\windows\nul path %PATH%;%USERDRV%:\windows
+echo Windows 3.1 was successfully installed.
+echo Type "win" and press enter to start Windows.
+goto end
+
+:faildownload
+echo There was an error downloading the required files to install Windows 3.1.
+goto end
+
+:failwinsetup
+echo The Windows 3.1 Setup program did not complete successfully.
+goto end
+
+:end
