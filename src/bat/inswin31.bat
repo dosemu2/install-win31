3ee5b968cce3f19f751efe0770d7b7177d645bc3
Julius Schwartzenberg <julius.schwartzenberg@eclipso.eu>
2025-02-10 16:15:44 +0100

    Install a localized version instead of the US English one

--- src/bat/inswin31.bat
+++ src/bat/inswin31.bat
@@ -1,8 +1,13 @@
 @echo off
 unix -s dosemu-preinstallwin31
 if errorlevel 1 goto faildownload
-if exist %USERDRV%:\inst\win31\setup.exe %USERDRV%:\inst\win31\setup.exe /h:f:\win31\dosemu.shh
+%USERDRV%:\tool\stuffkey\stuffkey.com /F:f:\stuffkey\w31skbsw.txt
+set W31SETUP=setup
+if not exist %USERDRV%:\inst\win31\%W31SETUP%.exe set W31SETUP=install
+if not exist %USERDRV%:\inst\win31\%W31SETUP%.exe goto failfindsetup
+%USERDRV%:\inst\win31\%W31SETUP%.exe /h:f:\win31\dosemu.shh
 if errorlevel 1 goto failwinsetup
+%USERDRV%:\tool\stuffkey\stuffkey.com /R > NUL
 if exist %USERDRV%:\windows\nul path %PATH%;%USERDRV%:\windows
 echo Windows 3.1 was successfully installed.
 echo Type "win" and press enter to start Windows.
@@ -12,7 +17,13 @@ goto end
 echo There was an error downloading the required files to install Windows 3.1.
 goto end
 
+:failfindsetup
+echo The executable to start Windows Setup could not be found.
+echo Neither setup.exe nor install.exe exists in %USERDRV%:\inst\win31
+goto end
+
 :failwinsetup
+%USERDRV%:\tool\stuffkey\stuffkey.com /R
 echo The Windows 3.1 Setup program did not complete successfully.
 goto end
 
