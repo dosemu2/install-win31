8ad80510b4b643f39bca61ef136354488569b97f
Stas Sergeev <stsp@users.sourceforge.net>
2025-03-02 14:09:49 +0300

    move away config.sys created by sb16 install
    It triggers the compatibility warning on dosemu2 startup.

--- src/bat/inswin31.bat
+++ src/bat/inswin31.bat
@@ -16,6 +16,8 @@ if exist %USERDRV%:\windows\system\midimap.cfg del %USERDRV%:\windows\system\mid
 if errorlevel 32 goto failsb16setup
 if not errorlevel 31 goto failsb16setup
 %USERDRV%:\tool\stuffkey\stuffkey.com /R > NUL
+rem SB16 install creates config.sys with drivers - move away
+if exist %USERDRV%:\config.sys move %USERDRV%:\config.sys %TEMP%
 echo Windows 3.1 was successfully installed.
 echo Type "win" and press enter to start Windows.
 goto end
